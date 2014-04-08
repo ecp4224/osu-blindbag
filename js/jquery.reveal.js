8b6dc7bf0a5e89f878f527bc735c8db612f3e17d
Eddie <eddie@gamezgalaxy.com>
2014-04-08 18:04:59 -0400

    Added popup window plugin and wheel now scrolls when clicked

--- js/jquery.reveal.js
+++ js/jquery.reveal.js
@@ -139,9 +139,6 @@
 				  modal.trigger('reveal:close')
 				});
 			}
-			$('body').keyup(function(e) {
-        		if(e.which===27){ modal.trigger('reveal:close'); } // 27 is the keycode for the Escape key
-			});
 			
 			
 /*---------------------------
--- js/osubb.js
+++ js/osubb.js
@@ -1,36 +1,95 @@
 function askForUsername() {
 	$("#usernameAsk").reveal({
 		animation: 'fade',
-		animationspeed: 450,
+		animationspeed: 444,
 		closeonbackgroundclick: false,
 	});
 }
 
+function shrekThem() {
+	$("#comebackLater").reveal({
+		animation: 'fade',
+		animationspeed: 444,
+		closeonbackgroundclick: false,
+	});
+}
+
+var username;
+var spunMap;
+var clicked = false;
+
+function spin(data, count, total) {
+	if (count <= 0) {
+		spunMap = data["maps"][Math.floor(Math.random() * (data["maps"].length - 1))];
+		$("#lwheel").text(spunMap.source + " (" + spunMap.artist + ") - " + spunMap.title);
+		$("#download").fadeIn(444);
+		$("#download").click(function() {
+			window.location.href = "dlredirect.html#" + spunMap.beatmapset_id;
+		});
+		$("#preview").fadeIn(444);
+		var song = new Sound("http://b.ppy.sh/preview/" + spunMap.beatmapset_id + ".mp3", 45, false);
+		$("#preview").click(function() {
+			song.start();
+		});
+		return;
+	}
+	
+	count--;
+	var time = ((count / total) * 2) - (((count / total) - 1) * 600);
+	var map = data["maps"][Math.floor(Math.random() * (data["maps"].length - 1))];
+	$("#lwheel").text(map.source + " (" + map.artist + ") - " + map.title);
+	setTimeout(function() {
+		spin(data, count, total);
+	}, time);
+}
+
 $(document).ready(function() {
 	$("#username_button").click(function() {
+		if ($("#username").val() == "") return;
+		username = $("#username").val();
+		if (typeof(Storage)!=="undefined") {
+			localStorage.setItem("user", username); 
+		}
+		
+		$.get("http://hypereddie.com/osubb/bag.php?action=canOpen&user=" + username, function(data) {
+			if (data == "false") {
+				shrekThem();
+			}
+		});
+		
 		$("#usernameAsk").trigger('reveal:close');
 	});
 	
 	$("#github_div").fadeTo(1, 0.3);
 	$("#github_div").hover(
 		function() {
-			$(this).fadeTo(320, 1);
+			$(this).fadeTo(333, 1);
 		},
 		function() {
-			$(this).fadeTo(320, 0.3);
+			$(this).fadeTo(333, 0.3);
 		}
 	);
 
 	$("#wheel_link").click(function() {
-		alert("This feature is not done yet.");
+		if (clicked) return;
+		$("#lwheel").text("Loading...");
+		clicked = true;
+		$.get("http://hypereddie.com/osubb/bag.php?action=open&user=" + username, function(data) {
+			var list = JSON.parse(data);
+			var num = list["selected"];
+			var count = Math.random() * (80 - 35) + 35;
+			setTimeout(function() {
+				spin(list, count, count);
+			}, 2);
+		});
 	});
 
 	if (typeof(Storage)!=="undefined") {
 		if (localStorage.getItem("user") !== null) {
-			var username = localStorage.getItem("user");
+			username = localStorage.getItem("user");
 			$.get("http://hypereddie.com/osubb/bag.php?action=canOpen&user=" + username, function(data) {
 				if (data == "false") {
-					askForUsername();
+					shrekThem();
 				}
 			});
 		} else {
@@ -41,3 +100,40 @@ $(document).ready(function() {
 	}
 	
 });
+
+function Sound(source,volume,loop)
+{
+    this.source=source;
+    this.volume=volume;
+    this.loop=loop;
+    var son;
+    this.son=son;
+    this.finish=false;
+    this.stop=function()
+    {
+        document.body.removeChild(this.son);
+    }
+    this.start=function()
+    {
+        if(this.finish)return false;
+        this.son=document.createElement("embed");
+        this.son.setAttribute("src",this.source);
+        this.son.setAttribute("hidden","true");
+        this.son.setAttribute("volume",this.volume);
+        this.son.setAttribute("autostart","true");
+        this.son.setAttribute("loop",this.loop);
+		this.son.setAttribute("style", "display: block"); //Stupid bootstrap..
+        document.body.appendChild(this.son);
+    }
+    this.remove=function()
+    {
+        document.body.removeChild(this.son);
+        this.finish=true;
+    }
+    this.init=function(volume,loop)
+    {
+        this.finish=false;
+        this.volume=volume;
+        this.loop=loop;
+    }
+}
